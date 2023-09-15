e7b1afed6582a0bce7f55e44403f9eedd5c00835
oscar <oscarpeters@live.nl>
2023-09-15 11:45:33 +0200

    Update contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -7,11 +7,11 @@ saveBtn.addEventListener("click", function (event) {
         phone: "0652334084",
         email: "patrick@latunyservices.nl",
         org: "Latuny Services",
-        img: "./assets/images/pasfoto.jpeg"
+        img: "assets/images/pasfoto.jpeg"
     };
     // create a vcard file
-    var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nTEL;TYPE=work,voice:" + contact.phone + "\nEMAIL:" + contact.email + "\nEND:VCARD";
-    // var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nORG:" + contact.org + `\nPHOTO;TYPE=JPEG;ENCODING=b:[${contact.img}]` +  "\nTEL;TYPE=work,voice:" + contact.phone  + "\nEMAIL:" + contact.email + "\nEND:VCARD";
+    // var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nTEL;TYPE=work,voice:" + contact.phone + "\nEMAIL:" + contact.email + "\nEND:VCARD";
+    var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nORG:" + contact.org + `\nPHOTO;TYPE=JPEG;ENCODING=b:[${contact.img}]` +  "\nTEL;TYPE=work,voice:" + contact.phone  + "\nEMAIL:" + contact.email + "\nEND:VCARD";
     var blob = new Blob([vcard], { type: "text/vcard" });
     var url = URL.createObjectURL(blob);
 
@@ -28,12 +28,15 @@ saveBtnMain.addEventListener("click", function (event) {
     event.preventDefault();
     // Get the contact information from the website
     var contact = {
-        name: "John Smith",
-        phone: "555-555-5555",
-        email: "john@example.com"
+        name: "Patrick Latuny",
+        phone: "0652334084",
+        email: "patrick@latunyservices.nl",
+        org: "Latuny Services",
+        img: "assets/images/pasfoto.jpeg"
     };
     // create a vcard file
-    var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nTEL;TYPE=work,voice:" + contact.phone + "\nEMAIL:" + contact.email + "\nEND:VCARD";
+    // var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nTEL;TYPE=work,voice:" + contact.phone + "\nEMAIL:" + contact.email + "\nEND:VCARD";
+    var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nORG:" + contact.org + `\nPHOTO;TYPE=JPEG;ENCODING=b:[${contact.img}]` +  "\nTEL;TYPE=work,voice:" + contact.phone  + "\nEMAIL:" + contact.email + "\nEND:VCARD";
     var blob = new Blob([vcard], { type: "text/vcard" });
     var url = URL.createObjectURL(blob);
 
