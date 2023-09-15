4e0e23c185425e0b911cd453389d6314764eaf52
oscar <oscarpeters@live.nl>
2023-09-15 11:49:42 +0200

    Update contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -7,11 +7,13 @@ saveBtn.addEventListener("click", function (event) {
         phone: "0652334084",
         email: "patrick@latunyservices.nl",
         org: "Latuny Services",
-        img: "assets/images/pasfoto.jpeg"
+        img: "assets/images/pasfoto.jpeg",
+        logo: "assets/images/latunyservices-logo.png"
+        
     };
     // create a vcard file
     // var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nTEL;TYPE=work,voice:" + contact.phone + "\nEMAIL:" + contact.email + "\nEND:VCARD";
-    var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nORG:" + contact.org + `\nPHOTO;TYPE=JPEG;ENCODING=b:[${contact.img}]` +  "\nTEL;TYPE=work,voice:" + contact.phone  + "\nEMAIL:" + contact.email + "\nEND:VCARD";
+    var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nORG:" + contact.org + `\nPHOTO;TYPE=JPEG;ENCODING=b:[${contact.img}]` + `\nLOGO; ENCODING = BASE64; TYPE = PNG: [${contact.logo}]` + "\nTEL;TYPE=work,voice:" + contact.phone  + "\nEMAIL:" + contact.email + "\nEND:VCARD";
     var blob = new Blob([vcard], { type: "text/vcard" });
     var url = URL.createObjectURL(blob);
 
