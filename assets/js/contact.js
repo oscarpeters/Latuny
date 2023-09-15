48606f8e0d638f5df70d2f53f15cb74409c35ba7
oscar <oscarpeters@live.nl>
2023-09-15 10:56:56 +0200

    button fix

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,14 +1,19 @@
+'use strict';
+
 var saveBtn = document.getElementById("save-btn");
 saveBtn.addEventListener("click", function () {
     // Get the contact information from the website
     var contact = {
-        name: "John Smith",
-        phone: "555-555-5555",
-        email: "john@example.com"
+        name: "Patrick Latuny",
+        phone: "0652334084",
+        email: "patrick@latunyservices.nl",
+        org: "Latuny Services",
+        img: "./assets/images/pasfoto.jpeg"
     };
     // create a vcard file
-    var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nTEL;TYPE=work,voice:" + contact.phone + "\nEMAIL:" + contact.email + "\nEND:VCARD";
-    var blob = new Blob([vcard], { type: "text/vcard" });
+    // var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nTEL;TYPE=work,voice:" + contact.phone + "\nEMAIL:" + contact.email + "\nEND:VCARD";
+    var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nORG:" + contact.org + `\nPHOTO;TYPE=JPEG;ENCODING=b:[${contact.img}]` +  "\nTEL;TYPE=work,voice:" + contact.phone  + "\nEMAIL:" + contact.email + "\nEND:VCARD";
+    var blob = new Blob([vcard], { type: "application/vcard" });
     var url = URL.createObjectURL(blob);
 
     const newLink = document.createElement('a');
