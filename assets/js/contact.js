068c528e6c04483823bd7d9dce68c97426eb6975
oscar <oscarpeters@live.nl>
2023-09-15 10:39:26 +0200

    Update contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -9,7 +9,8 @@ saveBtn.addEventListener("click", function () {
         img: `./assets/images/pasfoto.jpeg`
     };
     // create a vcard file
-    var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nORG" + contact.org + `\nPHOTO;TYPE=JPEG;ENCODING=b:[${contact.img}]` +  "\nTEL;TYPE=work,voice:" + contact.phone  + "\nEMAIL:" + contact.email + "\nEND:VCARD";
+    var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nTEL;TYPE=work,voice:" + contact.phone + "\nEMAIL:" + contact.email + "\nEND:VCARD";
+    // var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nORG" + contact.org + `\nPHOTO;TYPE=JPEG;ENCODING=b:[${contact.img}]` +  "\nTEL;TYPE=work,voice:" + contact.phone  + "\nEMAIL:" + contact.email + "\nEND:VCARD";
     var blob = new Blob([vcard], { type: "text/vcard" });
     var url = URL.createObjectURL(blob);
 
